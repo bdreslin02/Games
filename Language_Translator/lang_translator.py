ec586adae48a1cfe5dcef34def78422130369db3
Brandon Dreslin <bdreslin@gmail.com>
2022-05-26 11:37:34 -0400

    Update lang_translator.py

--- Language_Translator/lang_translator.py
+++ Language_Translator/lang_translator.py
@@ -2,10 +2,6 @@
 # Program Description: Automatically translates text from one lanuage into another. This is similar to Google Translate, but does not use a neural network to translate the text. Instead, this program uses the Google Translate
     # Ajax API 'googletrans' to make calls to the methods such as detect and translate the text.
     
-# Also Nicole, since I'm sending you the actual code, I figured I might as well explain the red text. So, the '#' followed by red texts are called 'comments.'
-    # Basically, they tell the person reading the code what the code does and anything that might need attention. This is a short little program,
-    # so there isn't a lot of code or comments.
-    
 # Import the 'googletrans' library.
 import googletrans
 # Import the 'Translator' module from 'googletrans' library.    
@@ -18,12 +14,7 @@ if avail_langs == 'y':
 # Print a groovy little space cause why not lmao.
 print()
 # Enter the text you want to be translated into the variable 'text' below. Make sure that there are three apostrophes (') before and after the text you would like translated. 
-source_text = '''Salut Nicole! Alors ... J'ai codé ce traducteur de langage en Python plus tôt dans la journée pour m'amuser parce que pourquoi pas, non?
-Je l'aime jusqu'à présent, même si ce n'est pas aussi pratique que Google Translate (principalement parce que je dois exécuter le code dans l'environnement de codage, pas sur Internet).
-Regardez ci-dessus, ce programme évalue également la confiance dans la détection et la traduction de la langue (en gros, à quel point il est sûr d'avoir fait la bonne chose)!
-C'est sur une échelle de 0 à 1, 1,0 étant une confiance extrême et absolue. Quoi qu'il en soit, je pensais que c'était une bonne chose que j'ai faite et je voulais juste vous montrer.
-Je t'aime tellement.
-(au fait, la traduction peut être légèrement décalée, donc si quelque chose ne va pas, ignorez-le hahaha. Je fait encore entraîner le programme)'''
+source_text = '''xxxxxxxx'''
 # Create an instance of the Google Translate API.
 translator = Translator()
 # Automatically detect the source language of the text.
